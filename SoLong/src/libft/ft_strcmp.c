3cf8c7633f07aebc31b764f80f2b38130f6577bc
Nako <nacho@quantumtemple.io>
2023-08-10 01:05:54 +0200

    change

--- SoLong/src/libft/ft_strcmp.c
+++ SoLong/src/libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: iggonzal <iggonzal@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:51:00 by iggonzal          #+#    #+#             */
-/*   Updated: 2023/08/06 13:43:19 by iggonzal         ###   ########.fr       */
+/*   Updated: 2023/08/10 01:04:58 by iggonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ int	ft_strcmp(const char *s1, const char *s2)
 		s1++;
 		s2++;
 	}
-	return (*(unsigned char*)s1 - *(unsigned char*)s2);
+	return (s1 - s2);
 }
